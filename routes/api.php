0d8c04b4960f985c867a16737678488d396bd0c4
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2026-04-08 23:37:02 +0300

    Move company settings routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\UserController;
+use App\Http\Controllers\Api\Company\CompanySettingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -21,6 +22,15 @@ Route::middleware('guest')->group(function () {
     Route::controller(LoginController::class)->group(function () {
         Route::post('login', 'login');
     });
+});
+
+Route::prefix('settings')->controller(CompanySettingsController::class)->group(function () {
+    Route::get('required-documents', 'getRequiredDocuments');
+    Route::get('activities', 'getActivities');
+    Route::get('governorates', 'getGovernorates');
+    Route::get('governorates/{governorate}/cities', 'getCities');
+    Route::get('nationalities', 'getNationalities');
+    Route::get('company-types', 'getCompanyTypes');
 });
 
 require_once __DIR__.'/api/admin.php';
--- routes/api/company.php
+++ routes/api/company.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\Company\CompanySettingsController;
 use App\Http\Controllers\Api\Company\CompanyTenderController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,15 +8,6 @@ Route::prefix('company')->middleware(['auth:sanctum', 'role:company_manager|comp
     Route::controller(CompanyTenderController::class)->group(function () {
         Route::get('tenders', 'index');
         Route::get('tenders/{tender}', 'show');
-    });
-
-    Route::prefix('settings')->controller(CompanySettingsController::class)->group(function () {
-        Route::get('required-documents', 'getRequiredDocuments');
-        Route::get('activities', 'getActivities');
-        Route::get('governorates', 'getGovernorates');
-        Route::get('governorates/{governorate}/cities', 'getCities');
-        Route::get('nationalities', 'getNationalities');
-        Route::get('company-types', 'getCompanyTypes');
     });
 
 });
